88cbcf7dcb859e2542d0f3f2e64cf5c562af0425
Yogesh Gite <yogesh.gite@hotmail.com>
2023-10-17 16:02:32 +0530

    Read file

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import fs from "fs"
+
+fs.writeFile("example.txt","hello world", (err)=>{
+    if(err) throw err;
+    console.log("Example file created.");
+
+    fs.readFile("example.txt",'utf-8', (err,data)=>{
+        if (err) throw err
+        console.log("File contents:");
+        console.log(data);
+    })
+
+})
+
+
+// if no err in code then err value will be null
