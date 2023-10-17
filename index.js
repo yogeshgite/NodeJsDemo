24caff26db31779312bb8a3e5fc7a512b27b088f
Yogesh Gite <yogesh.gite@hotmail.com>
2023-10-17 16:15:51 +0530

    Append and delete operation on file

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ fs.writeFile("example.txt","hello world", (err)=>{
     if(err) throw err;
     console.log("Example file created.");
 
-    fs.readFile("example.txt",'utf-8', (err,data)=>{
-        if (err) throw err
-        console.log("File contents:");
-        console.log(data);
-    })
+    // fs.readFile("example.txt",'utf-8', (err,data)=>{
+    //     if (err) throw err
+    //     console.log("File contents:");
+    //     console.log(data);
+    // })
 
+    fs.appendFile("example.txt","added new contents for demo", 'utf8', (err)=>{
+        if (err) throw err;
+        fs.readFile("example.txt",'utf-8', (err,data)=>{
+            if (err) throw err;
+            console.log("File contents:");
+            console.log(data);
+
+            fs.unlink("example.txt",(err)=>{
+                if (err) throw err;
+                console.log("File deleted successfully")
+            })
+         })
+    })
 })
 
 
